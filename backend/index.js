717c913fc6732a30693f06fddeadff182550815d
Yasintha28 <yasinthachamikara28@gmail.com>
2025-07-22 12:26:15 +0530

    Set up Express server and connect to MongoDB; add dotenv dependency

--- backend/index.js
+++ backend/index.js
@@ -1,1 +1,21 @@
-console.log("hello world");
+const express = require("express");
+const mongoose = require("mongoose");
+require("dotenv").config();
+
+const app = express();
+
+//Middleware
+app.use("/", (req, res, next) => {
+    res.send("It is working");
+})
+
+mongoose.connect(process.env.MONGO_URl)
+.then(()=>
+    console.log("Connected to DB")
+)
+.then(()=>{
+    app.listen(3000);
+})
+.catch((err)=>
+    console.log(err)
+)
