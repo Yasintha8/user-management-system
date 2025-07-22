04d7995ff30267f42bba8b946a5c96047775c5f2
Yasintha28 <yasinthachamikara28@gmail.com>
2025-07-22 19:00:27 +0530

    Enhance user management functionality: add user retrieval, update, and deletion routes; improve error handling in user operations.

--- backend/Controllers/UserController.js
+++ backend/Controllers/UserController.js
@@ -1,16 +1,15 @@
 import User from "../Models/UserModel.js";
 
 //--------Get all users-------
-export const getAllUsers = async (req, res) => {
+export const getAllUsers = async (req, res, next) => {
 
     try {
         const users = await User.find();
-        //if no users
         if(users.length === 0) {
-            return res.status(404).json({ message: "No users found" });
-        } //display users
+            return res.status(404).json({ message: "No users found" }); //if no users
+        } 
         else {
-            return res.status(200).json(users);
+            return res.status(200).json(users); 
         }
     } catch (error) {
         console.log("Error fetching users:", error);
@@ -19,16 +18,72 @@ export const getAllUsers = async (req, res) => {
 }
 
 //--------Create new user-------
-export const createUsers = async (req, res) => {
-    const { name, email, age, address } = req.body;
+export const createUsers = async (req, res, next) => {
+    const { name, email, age, address } = req.body; //Insert data
 
     try {
         const users = new User({ name, email, age, address });
         await users.save();
         console.log("User added successfully");
-        return res.status(201).json({ message: "User added successfully" });
+        return res.status(200).json({ message: "User added successfully" });
     }catch (error) {
         console.log(error);
         return res.status(400).json({ message: "Unable to add user" });
+    }
+}
+
+//--------Get By Id------------
+export const getUserById = async (req, res, next) => {
+    const id = req.params.id;
+
+    try {
+        const user = await User.findById(id);
+        if(!user) {
+            return res.status(404).json({ message: "User not found" }); //not available user
+        }
+        return res.status(200).json(user);  //display user
+    } catch (error) {
+        console.log(error);
+        return res.status(500).json({ message: "Internal Server Error" });
+    }
+}
+
+//----------Update User Details------------
+export const updateUser = async (req, res, next) => {
+    const id = req.params.id;
+    const { name, email, age, address } = req.body;
+
+    try {
+        const users = await User.findByIdAndUpdate(id);
+        if(!users){
+            return res.status(404).json({ message: "User not found" }); //not available user
+        }
+        users.name = name;
+        users.email = email;
+        users.age = age;
+        users.address = address;
+        await users.save();
+        return res.status(200).json({ message: "User updated successfully" });
+    } catch (error) {
+        console.log(error);
+        return res.status(500).json({ message: "Internal Server Error" });
+    }
+}
+
+//-----------Delete User------------
+export const deleteUser = async (req, res, next) => {
+    const id = req.params.id;
+
+    try {
+        const users  = await User.findByIdAndDelete(id);
+        if(!users){
+            return res.status(404).json({ message: "User not found" }); //not available user
+        }
+        else {
+            return res.status(200).json({ message: "User deleted successfully" });
+        }
+    } catch (error) {
+        console.log(error);
+        return res.json.status(500).json({ message: "Internal Server Error" });
     }
 }
--- backend/Routes/UserRoutes.js
+++ backend/Routes/UserRoutes.js
@@ -1,9 +1,12 @@
 import express from "express";
-import { createUsers, getAllUsers } from "../Controllers/UserController.js";
+import { createUsers, deleteUser, getAllUsers, getUserById, updateUser } from "../Controllers/UserController.js";
 
 const UserRouter = express.Router();
 
 UserRouter.get("/", getAllUsers);
 UserRouter.post("/", createUsers);
+UserRouter.get("/:id", getUserById);
+UserRouter.put("/:id", updateUser);
+UserRouter.delete("/:id", deleteUser);
 
 export default UserRouter;
