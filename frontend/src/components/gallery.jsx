35fde7b1748bd5edefa281160b518759a8f54a75
Yasintha28 <yasinthachamikara28@gmail.com>
2025-07-25 19:30:29 +0530

    Implement delete image in frontend

--- frontend/src/components/gallery.jsx
+++ frontend/src/components/gallery.jsx
@@ -1,6 +1,7 @@
 import axios from "axios";
 import { useEffect, useState } from "react";
 import toast from "react-hot-toast";
+import { FaTrashAlt } from "react-icons/fa";
 
 const Gallery = () => {
 
@@ -10,7 +11,7 @@ const Gallery = () => {
     const handleImageSubmit = async (e) => {
         e.preventDefault();
 
-         if (!image) {
+        if (!image) {
         toast.error("Please select an image before uploading.");
         return;
         }
@@ -56,6 +57,40 @@ const Gallery = () => {
         getImages();
     }, [])
 
+    // delete image
+    const handleDelete = async (id) => {
+        toast((t) => (
+          <span>
+            Are you sure, you want to delete this image?
+            <div className="flex gap-2 mt-2">
+            <button 
+              className="bg-red-600 text-white px-3 py-1 rounded hover:bg-red-700 cursor-pointer"
+              onClick={async ()=>{
+                toast.dismiss(t.id);
+                try {
+                  const response = await axios.delete(import.meta.env.VITE_BACKEND_URL + `/api/gallery/${id}`);
+                  console.log(response.data);
+                  toast.success("Image deleted successfully");
+                  getImages(); // Update the image list after deletion
+                } catch (error) {
+                  console.error(error);
+                  toast.error("Unable to delete image");
+                }
+              }}
+              >
+              Yes
+            </button>
+            <button 
+              className="bg-gray-600 text-white px-3 py-1 rounded hover:bg-gray-700 cursor-pointer"
+              onClick={() => toast.dismiss(t.id)}
+              >
+              No
+            </button>
+            </div>
+          </span>
+        ));
+    }
+
   return (
     <div className="min-h-[calc(100vh-60px)] bg-gray-100 p-4">
         <div className="max-w-md mx-auto bg-white p-6 rounded shadow mt-10">
@@ -73,19 +108,26 @@ const Gallery = () => {
 
         {/* display all images */}
         {allImages && (
-            <div className="mt-6">
-                <h2 className="text-2xl font-bold mb-4 text-center text-gray-700">Gallery</h2>
-                <div className="grid grid-cols-2 gap-4">
-                    {allImages.map((img) => (
-                        <div key={img._id} className="relative">
-                            <img 
-                                src={img.url} 
-                                alt="Uploaded"
-                                />
-                        </div>
-                    ))}
+        <div className="mt-6">
+            <h2 className="text-2xl font-bold mb-4 text-center text-gray-700">Gallery</h2>
+            <div className="grid grid-cols-2 gap-4">
+            {allImages.map((img) => (
+                <div key={img._id} className="relative group"> {/* Add group here */}
+                <img 
+                    src={img.url} 
+                    alt="Uploaded"
+                    className="w-full h-48 object-cover rounded shadow"
+                />
+                <button
+                    onClick={() => handleDelete(img._id)}
+                    className="absolute top-2 right-2 bg-red-600 text-white p-1 rounded-full hidden group-hover:block"
+                >
+                    <FaTrashAlt className="cursor-pointer"/>
+                </button>
                 </div>
+            ))}
             </div>
+        </div>
         )}
     </div>
     </div>
